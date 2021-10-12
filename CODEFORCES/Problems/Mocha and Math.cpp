63cab3d6314dae4531e32179eea046a052812447
Hasnath Jami Chowdhury <62906638+HasnathJami@users.noreply.github.com>
2021-10-12 21:09:16 +0600

    Add files via upload

---
+++ CODEFORCES/Problems/Mocha and Math.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define endl  "\n"
+#define nline cout<<"\n"
+#define FASTER  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define PI        acos(-1.0)
+#define pb push_back
+#define lb lower_bound
+#define imax INT_MAX
+#define imin INT_MIN
+#define llmax LLONG_MAX
+#define llmin LLONG_MIN
+
+int mod=1e9+7;
+
+
+int main ()
+{
+    FASTER
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n,storeMin=0;
+        cin>>n;
+
+        int a[n];
+        for(int i=0; i<n; i++)
+        {
+            cin>>a[i];
+        }
+
+        storeMin=a[0];
+
+        for(int i=1; i<n; i++)
+        {
+            storeMin=storeMin&a[i];
+        }
+
+
+        cout<<storeMin<<endl;
+
+
+
+
+
+    }
+
+
+
+
+    return 0;
+
+}
+
+
+
+
+
+
+
